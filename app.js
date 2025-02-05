675d5ba05e6b97c7dd03c821324a9eccb22d87b5
Ramon Monda <ramondriguess@gmail.com>
2025-02-05 12:43:25 -0300

    Update app.js
    Criado a função responsável por sortear um amigo na lista atual.

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ var listaDeAmigos = [];
 
 // Função responsável por adicionar nomes a lista
 function adicionarAmigo() {
+    let campoResultado = document.getElementById("resultado");
     let amigoParaAdicionar = document.getElementById("amigo");
 
     if (amigoParaAdicionar == null || amigoParaAdicionar.value == "") {
         alert("Por favor, insira um nome.");
     } else {
+        campoResultado.innerHTML = "";
         listaDeAmigos.push(amigoParaAdicionar.value);
         amigoParaAdicionar.value = "";
         console.log(listaDeAmigos);
@@ -26,6 +28,26 @@ function adicionarListaAoHtml() {
     }        
 }
 
+// Função responsável por sortear um amigo na lista atual
+function sortearAmigo() {
+    let listaDeExibicao = document.getElementById("listaAmigos");
+    let campoResultado = document.getElementById("resultado");
+
+    if (listaDeAmigos == null || listaDeAmigos.length == 0) {
+        alert("Lista de amigos vázia, adicione nomes para sortear.");
+    } else {
+        let numeroEscolhido = Math.floor(Math.random() * listaDeAmigos.length);
+        let amigoSorteado = listaDeAmigos[numeroEscolhido];
+        let novoItem = document.createElement("li");
+
+        listaDeExibicao.innerHTML = "";
+        campoResultado.innerHTML = "";        
+        novoItem.textContent = `O amigo sorteado foi "${amigoSorteado}".`;
+        campoResultado.appendChild(novoItem);
+        console.log(`O amigo sorteado foi "${amigoSorteado}".`);
+        listaDeAmigos = [];
+    }
+}
 
 
 
