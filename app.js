03c0956d93facc129f1f9dd28e847cc760ff3c8b
Ramon Monda <ramondriguess@gmail.com>
2025-02-05 11:42:20 -0300

    Update app.js
    Criado função responsável por adicionar os nomes da lista à tela

--- app.js
+++ app.js
@@ -5,15 +5,26 @@ function adicionarAmigo() {
     let amigoParaAdicionar = document.getElementById("amigo");
 
     if (amigoParaAdicionar == null || amigoParaAdicionar.value == "") {
-        alert("Digite um nome no campo para adicionar.");
+        alert("Por favor, insira um nome.");
     } else {
         listaDeAmigos.push(amigoParaAdicionar.value);
         amigoParaAdicionar.value = "";
         console.log(listaDeAmigos);
+        adicionarListaAoHtml();
     }
 }
 
+// Função responsável por adicionar os nomes da lista à tela
+function adicionarListaAoHtml() {
+    let listaDeExibicao = document.getElementById("listaAmigos");
+    listaDeExibicao.innerHTML = "";
 
+    for (let index = 0; index < listaDeAmigos.length; index++) {
+        let novoItem = document.createElement("li");
+        novoItem.textContent = listaDeAmigos[index];
+        listaDeExibicao.appendChild(novoItem);
+    }        
+}
 
 
 
